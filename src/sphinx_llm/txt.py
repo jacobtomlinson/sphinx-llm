7bd447e36c2db61c71b1a65d6f12dcfde9919923
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2025-12-19 09:37:47 +0000

    Add pre-commit to enforce SPDX headers (#51)

--- src/sphinx_llm/txt.py
+++ src/sphinx_llm/txt.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES.
+# All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 """
 Sphinx extension to generate markdown files alongside HTML files.
 
--- src/sphinx_llm/version.py
+++ src/sphinx_llm/version.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES.
+# All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 """Indirect import location for version info."""
 try:
     from ._version import version as __version__  # noqa
